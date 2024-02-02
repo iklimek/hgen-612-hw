65caf1d086572429355c5fb315382ce2dbde77ca
Isaiah Klimek <isaiah.klimek@gmail.com>
2024-02-01 23:55:21 -0500

    Creating a mtcars plot

---
+++ project-01.R
@@ -0,0 +1,15 @@
+# HGEN-612-project-01
+# RStudio and Github
+# author:  Isaiah Klimek            
+# due date: 2/2/24            
+
+# LIBRARIES
+library(tidyverse)
+library(ggplot2)
+
+#year vs mpg
+
+mtcars
+
+ggplot(mtcars, aes(wt,mpg)) +
+  geom_point(aes(color = cyl))
